a47532025911a5d2dc8f7e8f0e0d84404554af5b
André Marques <andre.m.d.abreu@gmail.com>
2023-10-04 22:25:47 -0300

    Função para inserir contatos

--- HTML/contato-tatuador.php
+++ HTML/contato-tatuador.php
@@ -1,13 +1,26 @@
-<?php require "./includes/cabecalho.php"; ?>
+<?php 
+    require "./includes/cabecalho.php";
+    require_once "./src/funcoes.php";
+
+    if(isset($_POST['add-att'])) {
+        $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_SPECIAL_CHARS);
+        $celular = filter_input(INPUT_POST, "celular", FILTER_SANITIZE_NUMBER_INT);
+        $telefone = filter_input(INPUT_POST, "telefone", FILTER_SANITIZE_NUMBER_INT);
+        $tatuadoresId = filter_input(INPUT_POST, "tatuadoresId", FILTER_SANITIZE_NUMBER_INT);
+
+        inserirContato($conexao, $email, $telefone, $celular, $tatuadoresId);
+    }
+?>
     <h1 class="titulo-cadastro">Contato</h1>
     <p><a class="botao-voltar" href="perfil-logado.php">Voltar</a></p>
     <form action="#" method="post" class="form-cadastro">
+        <input type="hidden" name="tatuadoresId" value="1">
         <div>
             <input type="text" name="email" id="email" placeholder="E-mail" required>
         </div>
         <section class="form-celular-telefone">
             <div>
-                <input type="number" name="celuar" id="celular" placeholder="Celular" required>
+                <input type="number" name="celular" id="celular" placeholder="Celular" required>
             </div>
             <div>
                 <input type="number" name="telefone" id="telefone" placeholder="Telefone">
--- HTML/src/funcoes.php
+++ HTML/src/funcoes.php
@@ -74,6 +74,34 @@ function inserirLocalizacao(PDO $conexao, string $cep, string $endereco, int $nu
     }
 }
 
+// função para inserir contato do tatuador
+function inserirContato(PDO $conexao, string $email, int $telefone, int $celular, int $tatuadoresId):void {
+    $sql = "INSERT INTO contatos(
+        email,
+        telefone,
+        celular,
+        tatuador_id
+    ) VALUES (
+        :email,
+        :telefone,
+        :celular,
+        :tatuador_id
+    )";
+
+    try {
+        $consulta = $conexao->prepare($sql);
+
+        $consulta->bindValue(":email", $email, PDO::PARAM_STR);
+        $consulta->bindValue(":telefone", $telefone, PDO::PARAM_INT);
+        $consulta->bindValue(":celular", $celular, PDO::PARAM_INT);
+        $consulta->bindValue(":tatuador_id", $tatuadoresId, PDO::PARAM_INT);
+
+        $consulta->execute();
+    } catch (Exception $erro) {
+        die("Erro ao inserir contatos: ".$erro->getMessage());
+    }
+}
+
 function atualizarLocalizacao(PDO $conexao, int $id, string $cep, string $endereco, int $numero, string $bairro, string $complemento) {
     $sql = "UPDATE tatuadores SET
     cep = :cep,
