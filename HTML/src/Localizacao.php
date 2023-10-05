b6467724eb4fd570023d653bedf12d6e2ad00b27
Marina Tanaka <huttenloucher@gmail.com>
2023-10-05 15:28:25 -0300

    Getters, Setters, Sanitização e criação do metodo inserir localização

---
+++ HTML/src/Localizacao.php
@@ -0,0 +1,129 @@
+<?php
+namespace Inkwizards;
+use PDO, Exception;
+
+
+class Localizacao {
+    // Propriedades da classe
+    private int $id;
+    private string $cep;
+    private string $endereco;
+    private int $numero;
+    private string $bairro;
+    private string $complemento;
+    private int $tatuadoresId;
+    
+
+    // Propriedade de conexao
+    private PDO $conexao;
+    // Metodo da conexão
+    public function __construct(){
+        $this->conexao = Banco::conecta();
+    }
+
+    // Metodo de inserir localização
+    public function inserirLocalizacao():void {
+        $sql = "INSERT INTO localizacao(
+            cep,
+            endereco,
+            numero,
+            bairro,
+            complemento,
+            tatuador_id
+        ) VALUES (
+            :cep,
+            :endereco,
+            :numero,
+            :bairro,
+            :complemento,
+            :tatuador_id
+        )";
+    
+        try {
+            $consulta = $this->conexao->prepare($sql);
+    
+            $consulta -> bindValue(":cep", $this->cep, PDO::PARAM_INT);
+            $consulta -> bindValue(":endereco", $this->endereco, PDO::PARAM_STR);
+            $consulta -> bindValue(":numero", $this->numero, PDO::PARAM_INT);
+            $consulta -> bindValue(":bairro", $this->bairro, PDO::PARAM_STR);
+            $consulta -> bindValue(":complemento", $this->complemento, PDO::PARAM_STR);
+            $consulta->bindValue(":tatuadores_id", $this->tatuadoresId, PDO::PARAM_INT);
+    
+            $consulta -> execute();
+        } catch (Exception $erro) {
+            die("Erro ao inserir localização: ".$erro->getMessage());
+        }
+    }
+
+   // Getters, Setters e Sanitização
+
+    // ID
+    public function getId():int {
+        return $this->id;
+    }
+
+    public function setId(int $id):self {
+        $this->id = filter_var($id, FILTER_SANITIZE_NUMBER_INT);
+        return $this;
+    }
+
+    // CEP
+    public function getCep():string {
+        return $this->cep;
+    }
+
+    public function setCep(int $cep):self {
+        $this->cep = filter_var($cep, FILTER_SANITIZE_SPECIAL_CHARS);
+        return $this;
+    }
+
+    // ENDEREÇO
+    public function getEndereco():string {
+        return $this->endereco;
+    }
+
+    public function setEndereco(string $endereco):self {
+        $this->endereco = filter_var($endereco, FILTER_SANITIZE_SPECIAL_CHARS);
+        return $this;
+    }
+
+    // NUMERO
+    public function getNumero():int {
+        return $this->numero;
+    }
+
+    public function setNumero(int $numero):self {
+        $this->numero = filter_var($numero, FILTER_SANITIZE_NUMBER_INT);
+        return $this;
+    }
+
+    // BAIRRO
+    public function getBairro():string {
+        return $this->bairro;
+    }
+
+    public function setBairro(string $bairro):self {
+        $this->bairro = filter_var($bairro, FILTER_SANITIZE_SPECIAL_CHARS);
+        return $this;
+    }
+
+    // COMPLEMENTO
+    public function getComplemento(): string {
+        return $this->complemento;
+    }
+
+    public function setComplemento(string $complemento): self {
+        $this->complemento = filter_var($complemento, FILTER_SANITIZE_SPECIAL_CHARS);
+        return $this;
+    }
+
+    // TATUADORES_ID
+    public function getTatuadoresId(): int {
+        return $this->tatuadoresId;
+    }
+
+    public function setTatuadoresId(int $tatuadoresId): self {
+        $this->tatuadoresId = $tatuadoresId;
+        return $this;
+    }
+}
--- HTML/src/Tatuador.php
+++ HTML/src/Tatuador.php
@@ -11,4 +11,66 @@ class Tatuador {
     private string $email;
     private string $senha;
     private int $localizacao_id;
+
+    // Getters, Setters e Sanitização
+
+    // ID
+    public function getId(): int {
+        return $this->id;
+    }
+
+    public function setId(int $id): self {
+        $this->id = $id;
+        return $this;
+    }
+
+    // NOME
+    public function getNome(): string {
+        return $this->nome;
+    }
+
+    public function setNome(string $nome): self {
+        $this->nome = $nome;
+        return $this;
+    }
+
+    // DESCRIÇÃO
+    public function getDescricao(): string {
+        return $this->descricao;
+    }
+
+    public function setDescricao(string $descricao): self {
+        $this->descricao = $descricao;
+        return $this;
+    }
+    
+    // E-MAIL
+    public function getEmail(): string {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self {
+        $this->email = $email;
+        return $this;
+    }
+
+    // SENHA
+    public function getSenha(): string {
+        return $this->senha;
+    }
+
+    public function setSenha(string $senha): self {
+        $this->senha = $senha;
+        return $this;
+    }
+
+   
+    public function getLocalizacaoId(): int {
+        return $this->localizacao_id;
+    }
+
+    public function setLocalizacaoId(int $localizacao_id): self {
+        $this->localizacao_id = $localizacao_id;
+        return $this;
+    }
 }
