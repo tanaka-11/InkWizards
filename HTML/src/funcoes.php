15ad07e2e1b6eb6516b044ff68532967b4c0cc10
Marina Tanaka <huttenloucher@gmail.com>
2023-10-09 08:44:31 -0300

    update semantico

--- HTML/src/funcoes.php
+++ HTML/src/funcoes.php
@@ -1,34 +1,6 @@
 <?php
 require_once "conecta.php";
 
-// função para inserir contato do tatuador
-function inserirContato(PDO $conexao, string $email, int $telefone, int $celular, int $tatuadoresId):void {
-    $sql = "INSERT INTO contatos(
-        email,
-        telefone,
-        celular,
-        tatuador_id
-    ) VALUES (
-        :email,
-        :telefone,
-        :celular,
-        :tatuador_id
-    )";
-
-    try {
-        $consulta = $conexao->prepare($sql);
-
-        $consulta->bindValue(":email", $email, PDO::PARAM_STR);
-        $consulta->bindValue(":telefone", $telefone, PDO::PARAM_INT);
-        $consulta->bindValue(":celular", $celular, PDO::PARAM_INT);
-        $consulta->bindValue(":tatuador_id", $tatuadoresId, PDO::PARAM_INT);
-
-        $consulta->execute();
-    } catch (Exception $erro) {
-        die("Erro ao inserir contatos: ".$erro->getMessage());
-    }
-}
-
 function atualizarLocalizacao(PDO $conexao, int $id, string $cep, string $endereco, int $numero, string $bairro, string $complemento) {
     $sql = "UPDATE tatuadores SET
     cep = :cep,
