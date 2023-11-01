90444bd8e361fda97ce55609277d6a9930632c8e
Marina Tanaka <huttenloucher@gmail.com>
2023-11-01 12:01:32 -0300

    refatoração dos caminhos

--- admin/portfolios.php
+++ admin/portfolios.php
@@ -7,28 +7,19 @@ $portfolio = new Portfolio;
 $dadosPortfolio = $portfolio->exibir();
 ?>
 
-    <h1>Estilos</h1>
-    <table class="table table-striped table-hover">
-            <thead>
-                <tr>
-                    <th>Nome</th>
-                    <th>Imagem</th>
-                    <th>Descrição</th>
-                    <th class="text-center">Operações</th>
-                </tr>
-            </thead>
+    <section class="container">
+    <?php foreach($dadosPortfolio as $umPortfolio){?>
+        <div id="portfolio">
+            <img src="../assets/images/portfolio/<?=$umPortfolio['imagem']?>" alt="Imagem Portfolio">
+            <a href="portfolio-atualizar.php">E</a>
+        </div>
+        
+        <!-- <h1 class="nome-portfolio">
+            <?=$umPortfolio['']?> <a href="portfolio-atualizar.php">E</a>
+        </h1> -->
 
-    <tbody class="table-group-divider">
-    <?php foreach($dadosPortfolio as $umPortfolio) {?>
-                <tr>
-                    <td><?=$umPortfolio['nome']?></td>
-                    <td><?=$umPortfolio['imagem']?></td>
-                    <td><?=$umPortfolio['descricao']?></td>
-    
-                    <td class="text-center">
-                        <a class="btn btn-warning" href="usuario-atualizar.php?id=<?=$umPortfolio['id']?>">Atualizar</a>
-                        <a class="btn btn-danger" href="usuario-excluir.php?id=<?=$umPortfolio['id']?>">Excluir</a>
-                    </td>
-                </tr>
-    <?php }?>
-    </table>
+        <p>
+            <?=$umPortfolio['descricao']?> <a href="portfolio-atualizar.php">E</a>
+        </p>
+    <?php } ?>    
+    </section>
