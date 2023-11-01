88c028578a1575690c47c197a528112357bcd4e1
André Marques <andre.m.d.abreu@gmail.com>
2023-11-01 10:11:34 -0300

    Início da página de atualizar usuário

---
+++ admin/usuario-atualizar.php
@@ -0,0 +1,11 @@
+<?php
+    require_once "../inc/cabecalho-admin.php";
+?>
+<section class="container">
+    <h1 class="text-center">Atualizar usuário</h1>
+    <form action="#" method="post">
+        <div class="form-floating m-3">
+            <input class="form-control" type="file" name="" id="">
+        </div>
+    </form>
+</section>
