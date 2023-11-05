8d561259dd61c38b4dd75de707945c7d1f2d0ebc
Marina Tanaka <huttenloucher@gmail.com>
2023-11-05 09:37:08 -0300

    Adcionado estilos na parte de portfolios.php

--- admin/portfolios.php
+++ admin/portfolios.php
@@ -5,19 +5,28 @@ require_once "../inc/cabecalho-admin.php";
 $portfolio = new Portfolio;
 
 $dadosPortfolio = $portfolio->exibir();
+$dadosEstilo = $portfolio->estilo->exibir();
 ?>
 
 <section class="container">
+
 <?php foreach($dadosPortfolio as $umPortfolio){?>    
         <div id="imagemPortfolio">
             <img src="../assets/images/portfolio/<?=$umPortfolio['imagem']?>" alt="Foto do portfolio">
             <a href="portfolio-atualizar.php?id=<?=$umPortfolio['id']?>">Editar</a>
         </div>
 
-        <h2>
+        <p>
             <?=$umPortfolio['descricao']?> <a href="portfolio-atualizar.php?id=<?=$umPortfolio['id']?>">Editar</a>
-        </h2>
-        <?php } ?>
+        </p> 
+
+        <?php foreach($dadosEstilo as $umEstilo){?>
+            <p value="<?=$umEstilo['id']?>"><?=$umEstilo['nome']?></p>
+        <?php }?>
+       
+
+<?php } ?>
+        
     </section>    
 <?php 
 require_once "../inc/rodape.php";
