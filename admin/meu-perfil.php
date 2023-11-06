7de10d898254aeff25d6f3ef7060105b6fa7b934
André Marques <andre.m.d.abreu@gmail.com>
2023-11-06 11:37:02 -0300

    Update meu-perfil.php

--- admin/meu-perfil.php
+++ admin/meu-perfil.php
@@ -1,5 +1,5 @@
 <?php
-use Inkwizards\{Usuario, Localizacao, Contato};
+use Inkwizards\{Usuario, Localizacao, Contato, Portfolio};
 require "../inc/cabecalho-admin.php";
 
 $usuario = new Usuario;
@@ -12,6 +12,11 @@ $dadosLocalizacao = $localizacao->exibirUm();
 $contato = new Contato;
 $contato->usuario->setId($_SESSION['id']);
 $dadosContato = $contato->exibirUm();
+
+$portfolio = new Portfolio;
+$portfolio->usuario->setId($_SESSION['id']);
+$dadosPortfolio = $portfolio->exibirUsuario();
+$dadosEstilos = $portfolio->estilo->exibir();
 
 $dadosUsuario = $usuario->exibirUm();
 ?>
@@ -42,6 +47,20 @@ $dadosUsuario = $usuario->exibirUm();
             <p><?=$dadosContato['celular']?> <a class="btn btn-primary" href="contato-atualizar.php?id=<?=$dadosContato['id']?>">E</a></p>
         <?php } ?>
         <p><a class="btn btn-primary" href="portfolio-inserir.php">Adicionar portfolio</a></p>
+
+        <?php foreach($dadosPortfolio as $umPortfolio){ ?>
+            <div class="card m-3">
+                <div class="card-header">
+                    <p><?=$umPortfolio['descricao']?></p>
+                </div>
+                <div class="card-body">
+                    <p><?=$dadosEstilos[$umPortfolio['estilo_id'] - 1]['nome']?></p>
+                    <p><a class="btn btn-warning" href="portfolio-atualizar.php?id=<?=$umPortfolio['id']?>">Atualizar</a></p>
+                    <p><a class="btn btn-danger" href="portfolio-excluir.php?id=<?=$umPortfolio['id']?>">Excluir</a></p>
+                    <img src="../assets/images/portfolio/<?=$umPortfolio['imagem']?>" alt="Foto tatuagem" class="card-img-bottom">
+                </div>
+            </div>
+        <?php } ?>
         
     </section>
 
