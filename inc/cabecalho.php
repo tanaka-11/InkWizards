b855b726da83cd31b46f3f0ed1a0f076627da68a
Marina Tanaka <huttenloucher@gmail.com>
2023-11-06 08:36:37 -0300

    pequeno ajuste nos cabeçalhos

--- inc/cabecalho.php
+++ inc/cabecalho.php
@@ -22,7 +22,7 @@ $titulo = "InkWizards";
 $titulo = match($pagina) {
     "index.php" => TITULO_INICIAL,
     "estilos.php" => TITULO_ESTILOS,
-    "contato.php" => TITULO_CONTATO,
+    "contatos.php" => TITULO_CONTATO,
     "login.php" => TITULO_LOGIN,
     default => $titulo // Valor padrão
 };
@@ -39,7 +39,7 @@ $titulo = match($pagina) {
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title><?=$titulo?> - InkWizards</title>
+    <title><?=$titulo?></title>
 
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
