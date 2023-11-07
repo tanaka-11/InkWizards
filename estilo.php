c12eaca82466d1198165dd9e0d93afe2b9f0b6d0
Marina Tanaka <huttenloucher@gmail.com>
2023-11-07 15:28:15 -0300

    mudança na cor dos cards

--- estilo.php
+++ estilo.php
@@ -9,7 +9,7 @@ $dadosEstilo = $estilo->exibir();
     <h1 class="text-center m-3">Estilos</h1>
     <section class="d-flex justify-content-center container">
 <?php foreach($dadosEstilo as $umEstilo) {?>    
-    <div class="card w-75 m-3 text-center">
+    <div class="card w-75 m-3 text-center text-bg-dark ">
         <div class="card-body">
             <h5 class="card-title"><?=$umEstilo['nome']?></h5>
             <a href="#" class="btn btn-outline-danger">Acesse todas tatuagens deste estilo</a>
