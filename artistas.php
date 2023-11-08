91ac772398093e56f8c10959726838db49df04ea
Marina Tanaka <huttenloucher@gmail.com>
2023-11-08 20:44:11 -0300

    atualização no front-end de artistas

--- artistas.php
+++ artistas.php
@@ -12,32 +12,32 @@ $dadosUsuario = $usuario->exibir();
 ?>
 
 <h1 class="text-center m-3"> Nossos Artistas</h1>
+<section class="container">
+    <div class="row">
+        <?php foreach ($dadosUsuario as $umUsuario) { ?>
+            <?php if ($umUsuario['tipo'] === 'tatuador') { ?>
+                <div class="col-12 col-md-6 col-lg-4">
+                    <div class="card text-bg-dark m-4">
+                        <div class="row g-0">
+                            <div class="col-md-7">
+                                <img src="assets/images/perfil/<?= $umUsuario['foto_perfil'] ?>" class="card-img foto-portfolio w-100" alt="Imagem do perfil">
+                            </div>
 
-<section class="d-flex justify-content-center container">
-    <?php foreach($dadosUsuario as $umUsuario){ ?>
-        <?php if($umUsuario['tipo'] === 'tatuador'){ ?>
-        <div class="card m-3 text-bg-dark " style="width: 650px;">
-            <div class="row g-0">
-                <div class="col-md-4">
-                <img  src="assets/images/perfil/<?=$umUsuario['foto_perfil'];?>" class="img-fluid rounded-start foto-portfolio" alt="Imagem do perfil">
-                </div>
+                            <div class="col-md-5">
+                                <div class="card-body">
+                                    <h5 class="card-title"><?= $umUsuario['nome'] ?></h5>
+                                    <p class="card-text"><?= $umUsuario['descricao'] ?></p>
+                                </div>
 
-                <div class="col-md-8">
-                <div class="card-body">
-                    <h5 class="card-title"><?=$umUsuario['nome']?></h5>
-                    <p class="card-text"><?=$umUsuario['descricao']?></p>
+                                <div class="card-body text-center">
+                                    <a href="artista.php?id=<?= $umUsuario['id'] ?>" class="btn btn-outline-danger">Veja mais</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-
-                <!-- FAZER METODO PARA APARECER PORTFOLIO (Na classe Usuario) -->
-                
-                <div class="card-body">
-                    <a href="artista.php?id=<?=$umUsuario['id']?>" class="btn btn-outline-danger">Clique para ver mais trabalhos</a>
-                </div>
-                </div>
-            </div>
-        </div>
-        <?php }?>
-    <?php } ?>
+            <?php } ?>
+        <?php } ?>
+    </div>
 </section>
-
 <?php require_once 'inc/rodape.php'; ?>
