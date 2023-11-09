5973e29f6f1cde30cff4c3eb9a88d6ca654d5f72
Marina Tanaka <huttenloucher@gmail.com>
2023-11-08 23:26:33 -0300

    ultimo ajuste

--- estilo.php
+++ estilo.php
@@ -11,12 +11,15 @@ use Inkwizards\Portfolio;
 <section class="portfolio">
     <h2 class="text-center m-3 p-3">Tatuagens</h2>
     <div class="container">
-        <p><a class="btn btn-danger" href="estilos.php">Voltar</a></p>
+        <p class="text-center"><a class="btn btn-danger" href="estilos.php">Voltar</a></p>
         <div class="row row-cols-1 row-cols-md-3 g-4">
+            <?php if (count($dadosPortfolio) === 1) { ?>
+                <div class="col col-md-6 mx-auto">
+            <?php } ?>
             <?php foreach ($dadosPortfolio as $umPortfolio) { ?>
                 <div class="col mb-4">
                     <div class="card text-bg-dark">
-                        <img src="assets/images/portfolio/<?= $umPortfolio['imagem'] ?>" class="card-img foto-portfolio align-self-center w-100" alt="...">
+                        <img src="assets/images/portfolio/<?= $umPortfolio['imagem'] ?>" class="card-img foto-portfolio w-100 h-100" alt="..." style="min-height: 460px;">
 
                         <div class="card-img-overlay">
                             <h5 class="card-title"><?= $umPortfolio['estilo'] ?></h5>
@@ -29,4 +32,5 @@ use Inkwizards\Portfolio;
         </div>
     </div>
 </section>
+
 <?php require_once "inc/rodape.php"; ?>
